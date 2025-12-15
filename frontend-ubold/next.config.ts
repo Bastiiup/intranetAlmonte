edc48afd4dd4a44ceb631a877c0c295c91f0aca6
subimeDev <riquelmemedinaluismatias@gmail.com>
2025-12-15 18:58:36 -0300

    Reorganizar estructura: mover frontend-ubold a subcarpeta

---
+++ frontend-ubold/next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ];
+  },
+  sassOptions: {
+    includePaths: [
+      './src/assets/scss',
+      './node_modules/bootstrap/scss',
+    ],
+    silenceDeprecations: ['legacy-js-api'],
+  },
+};
+
+export default nextConfig;
