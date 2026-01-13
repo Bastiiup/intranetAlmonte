327cfce2e2283de27ea56e5be0c5bef9fb48c3bf
subimedev <riquelmemedinaluismatias@gmail.com>
2026-01-13 10:08:17 -0300

    Corregir PATH en iniciar.bat para que npm encuentre node en scripts internos

--- AlmonteIntranet/iniciar.bat
+++ AlmonteIntranet/iniciar.bat
@@ -4,18 +4,22 @@ echo.
 
 REM Buscar Node.js
 set "NODEJS="
+set "NODEJS_DIR="
 if exist "C:\Program Files\nodejs\node.exe" (
     set "NODEJS=C:\Program Files\nodejs\node.exe"
+    set "NODEJS_DIR=C:\Program Files\nodejs"
     set "NPM=C:\Program Files\nodejs\npm.cmd"
     goto :found
 )
 if exist "C:\Program Files (x86)\nodejs\node.exe" (
     set "NODEJS=C:\Program Files (x86)\nodejs\node.exe"
+    set "NODEJS_DIR=C:\Program Files (x86)\nodejs"
     set "NPM=C:\Program Files (x86)\nodejs\npm.cmd"
     goto :found
 )
 if exist "%USERPROFILE%\AppData\Roaming\npm\node.exe" (
     set "NODEJS=%USERPROFILE%\AppData\Roaming\npm\node.exe"
+    set "NODEJS_DIR=%USERPROFILE%\AppData\Roaming\npm"
     set "NPM=%USERPROFILE%\AppData\Roaming\npm\npm.cmd"
     goto :found
 )
@@ -25,6 +29,9 @@ pause
 exit /b 1
 
 :found
+REM Agregar Node.js al PATH para que npm pueda encontrar node en scripts internos
+set "PATH=%NODEJS_DIR%;%PATH%"
+
 REM Verificar dependencias
 if not exist "node_modules" (
     echo Instalando dependencias...
